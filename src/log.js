50407c80efcd3ea5761346d0c2c9ac4338f375b4
Ken Mueller <kenmueller0@gmail.com>
2023-04-23 17:34:46 -0700

    log types

--- src/log.js
+++ src/log.js
@@ -1,5 +1,6 @@
 const chalk = require('chalk')
 
+/** @type {(prefix: string, message: 'string', status: 'success' | 'error') => void} */
 const log = (prefix, message, status) => {
 	console[status === 'success' ? 'log' : 'error'](
 		chalk[status === 'success' ? 'green' : 'red'](prefix + message)
